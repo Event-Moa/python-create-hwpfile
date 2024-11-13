5fe8225feb04878df54891d15b68556742ee2c0b
leesu9042 <tnckddl8957@gmail.com>
2024-11-13 17:08:50 +0900

    hwpDepartmentCreate.py 코드수정

--- pythonHwp/python_test.py
+++ pythonHwp/python_test.py
@@ -1,7 +1,21 @@
 from pythonHwp.hwpDeparmentCreate import EventTableProcessor
 
-#파일 save할떄 만약 이미 같은 경로에 파일이 있다면 warning이라든지 클라에게 표시를 해줘야함
+# processor = EventTableProcessor(hwp_path="../template2.hwp", excel_path="../eventdummy.xlsx")  # 인자값에 한글path랑 엑셀 패스 넣어줌
+# processor.process() #한글 생성 함수
+# processor.save_hwp_file("final_output.hwp")  # 파일을 "final_output.hwp" 이름으로 저장
 
-processor = EventTableProcessor(hwp_path="../template2.hwp", excel_path="../eventdummy.xlsx")  # 인자값에 한글path랑 엑셀 패스 넣어줌
-processor.process() #한글 생성 함수
-processor.save_hwp_file("final_output.hwp")  # 파일을 "final_output.hwp" 이름으로 저장
+
+# hwp_file = r"../template2.hwp"
+# excel_file = r"../eventdummy.xlsx"
+#
+# event_processor = EventProcessor(hwp_file, excel_file)
+# event_processor.process()
+
+
+
+# 인스턴스 생성
+processor = EventTableProcessor("../template2.hwp", "../eventdummy.xlsx")
+
+# 모든 작업을 한 번에 처리
+processor.process()
+
